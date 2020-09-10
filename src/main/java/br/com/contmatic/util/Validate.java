ec25d48b8ab88bc0ff4b2cf99bb78b796271af27
gabrielbms <gabriel.bueno.m.s@hotmail.com.br>
2020-09-10 15:52:22 -0300

    Adaptações
    Alterei o nome das variaveis para ficar mais legivel.

--- src/main/java/br/com/contmatic/util/Validate.java
+++ src/main/java/br/com/contmatic/util/Validate.java
@@ -13,36 +13,36 @@ public class Validate {
 			return (false);
 
 		char dig10, dig11;
-		int sm, i, r, num, peso;
+		int soma, i, resultado, num, peso;
 
 		try {
-			sm = 0;
+			soma = 0;
 			peso = 10;
 			for (i = 0; i < 9; i++) {
 				num = (int) (CPF.charAt(i) - 48);
-				sm = sm + (num * peso);
+				soma = soma + (num * peso);
 				peso = peso - 1;
 			}
 
-			r = 11 - (sm % 11);
-			if ((r == 10) || (r == 11))
+			resultado = 11 - (soma % 11);
+			if ((resultado == 10) || (resultado == 11))
 				dig10 = '0';
 			else
-				dig10 = (char) (r + 48);
+				dig10 = (char) (resultado + 48);
 
-			sm = 0;
+			soma = 0;
 			peso = 11;
 			for (i = 0; i < 10; i++) {
 				num = (int) (CPF.charAt(i) - 48);
-				sm = sm + (num * peso);
+				soma = soma + (num * peso);
 				peso = peso - 1;
 			}
 
-			r = 11 - (sm % 11);
-			if ((r == 10) || (r == 11))
+			resultado = 11 - (soma % 11);
+			if ((resultado == 10) || (resultado == 11))
 				dig11 = '0';
 			else
-				dig11 = (char) (r + 48);
+				dig11 = (char) (resultado + 48);
 
 			if ((dig10 == CPF.charAt(9)) && (dig11 == CPF.charAt(10)))
 				return (true);
@@ -61,40 +61,40 @@ public class Validate {
 			return (false);
 
 		char dig13, dig14;
-		int sm, i, r, num, peso;
+		int soma, i, resultado, num, peso;
 
 		try {
-			sm = 0;
+			soma = 0;
 			peso = 2;
 			for (i = 11; i >= 0; i--) {
 				num = (int) (CNPJ.charAt(i) - 48);
-				sm = sm + (num * peso);
+				soma = soma + (num * peso);
 				peso = peso + 1;
 				if (peso == 10)
 					peso = 2;
 			}
 
-			r = sm % 11;
-			if ((r == 0) || (r == 1))
+			resultado = soma % 11;
+			if ((resultado == 0) || (resultado == 1))
 				dig13 = '0';
 			else
-				dig13 = (char) ((11 - r) + 48);
+				dig13 = (char) ((11 - resultado) + 48);
 
-			sm = 0;
+			soma = 0;
 			peso = 2;
 			for (i = 12; i >= 0; i--) {
 				num = (int) (CNPJ.charAt(i) - 48);
-				sm = sm + (num * peso);
+				soma = soma + (num * peso);
 				peso = peso + 1;
 				if (peso == 10)
 					peso = 2;
 			}
 
-			r = sm % 11;
-			if ((r == 0) || (r == 1))
+			resultado = soma % 11;
+			if ((resultado == 0) || (resultado == 1))
 				dig14 = '0';
 			else
-				dig14 = (char) ((11 - r) + 48);
+				dig14 = (char) ((11 - resultado) + 48);
 
 			if ((dig13 == CNPJ.charAt(12)) && (dig14 == CNPJ.charAt(13)))
 				return (true);
